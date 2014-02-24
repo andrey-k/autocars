ca74f39d062d6f8a62c709bfa829cf41f61f9ebb
andrey-k <andrey.kramarev@gmail.com>
2014-02-25 00:01:17 +0200

    changed represantation on admin page

--- django/autocars/sections/admin.py
+++ django/autocars/sections/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from models import SpecialOffer
+from models import Service
+from models import Worker
+from models import Testimonial
 
 
-admin.site.register(SpecialOffer)
+class SpecialOfferAdmin(admin.ModelAdmin):
+    list_display = ('title', 'is_active', 'is_week_offer')
+
+admin.site.register(SpecialOffer, SpecialOfferAdmin)
+admin.site.register(Service)
+admin.site.register(Worker)
+admin.site.register(Testimonial)
