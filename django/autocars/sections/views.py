9049a5ce4406135e0b8340edbe3cf1bcb65564a6
andrey-k <andrey.kramarev@gmail.com>
2014-02-25 00:02:55 +0200

    first templates and views

--- django/autocars/sections/views.py
+++ django/autocars/sections/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import get_list_or_404
 
-# Create your views here.
+from autocars.sections.models import SpecialOffer
+
+
+def index(request):
+    offers = get_list_or_404(SpecialOffer)
+
+    return render(request, 'sections/index.html', {'offers': offers})
--- django/autocars/urls.py
+++ django/autocars/urls.py
@@ -3,11 +3,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'autocars.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = patterns(
+    '',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^tinymce/', include('tinymce.urls')),
+    url(r'^$', include('autocars.sections.urls', namespace='sections')),
 )
