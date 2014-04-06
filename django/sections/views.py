f8ce27d707b3b5c05b918edf29e41d3fd2a07409
andrey-k <andrey.kramarev@gmail.com>
2014-04-06 15:37:38 +0300

    added services section and images for that section

--- django/sections/views.py
+++ django/sections/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import get_list_or_404
 
 from sections.models import SpecialOffer
+from sections.models import Service
 
 
 def index(request):
@@ -9,6 +10,8 @@ def index(request):
         is_active=True, is_week_offer=True).first()
     special_offers = SpecialOffer.objects.filter(
         is_active=True, is_week_offer=False)[:3]
-    
+
+    services = Service.objects.all()
+
     return render(request, 'sections/index.html',
-                  {'week_offer': week_offer, 'special_offers': special_offers})
+                  {'week_offer': week_offer, 'special_offers': special_offers, 'services': services})
