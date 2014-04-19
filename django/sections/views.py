5daa52b6fbecc3580fe9c18c4158cfa1b1ae86ed
andrey-k <andrey.kramarev@gmail.com>
2014-04-19 17:24:09 +0300

    added testimonials block

--- django/sections/views.py
+++ django/sections/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_list_or_404
 
 from sections.models import SpecialOffer
 from sections.models import Service
+from sections.models import Testimonial
 
 
 def index(request):
@@ -13,5 +14,8 @@ def index(request):
 
     services = Service.objects.all()
 
+    testimonials = Testimonial.objects.all()
+
     return render(request, 'sections/index.html',
-                  {'week_offer': week_offer, 'special_offers': special_offers, 'services': services})
+                  {'week_offer': week_offer, 'special_offers': special_offers,
+                   'services': services, 'testimonials': testimonials})
